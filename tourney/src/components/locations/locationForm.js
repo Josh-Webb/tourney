f373b832ea2255fbac483bc2e611772675bef07f
Josh Webb <joshwebb42@gmail.com>
2019-06-19 15:19:13 -0500

    got checkboxes working on location. Will implement them into users

--- tourney/src/components/locations/locationForm.js
+++ tourney/src/components/locations/locationForm.js
@@ -3,19 +3,71 @@ import React, { Component } from 'react';
 import LocationsManager from '../../modules/LocationsManager';
 import { Media, Button, Form, FormGroup, Label, Input, FormText, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import './locations.css';
+import Checkbox from "./locationCheckbox";
+
+const OPTIONS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
 export default class EventForm extends Component {
   // Set initial state
   state = {
     address: '',
-    monday: '',
-    tuesday: '',
-    wednesday: '',
-    thursday: '',
-    friday: '',
-    saturday: '',
-    sunday: ''
+    checkboxes: OPTIONS.reduce(
+        (options, option) => ({
+            ...options,
+            [option]: false
+        }),
+        {}
+    )
   };
+
+  selectAllCheckboxes = isSelected => {
+    Object.keys(this.state.checkboxes).forEach(checkbox => {
+      // BONUS: Can you explain why we pass updater function to setState instead of an object?
+      this.setState(prevState => ({
+        checkboxes: {
+          ...prevState.checkboxes,
+          [checkbox]: isSelected
+        }
+      }));
+    });
+  };
+
+  selectAll = () => this.selectAllCheckboxes(true);
+
+  deselectAll = () => this.selectAllCheckboxes(false);
+
+  handleCheckboxChange = changeEvent => {
+    const { name } = changeEvent.target;
+
+    this.setState(prevState => ({
+      checkboxes: {
+        ...prevState.checkboxes,
+        [name]: !prevState.checkboxes[name]
+      }
+    }));
+  };
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    Object.keys(this.state.checkboxes)
+      .filter(checkbox => this.state.checkboxes[checkbox])
+      .forEach(checkbox => {
+        console.log(checkbox, "is selected.");
+      });
+  };
+
+  createCheckbox = option => (
+    <Checkbox
+      label={option}
+      isSelected={this.state.checkboxes[option]}
+      onCheckboxChange={this.handleCheckboxChange}
+      key={option}
+    />
+  );
+
+  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
 
   addLocation = location =>
     LocationsManager.post(location)
@@ -31,22 +83,20 @@ export default class EventForm extends Component {
   handleFieldChange = evt => {
     const stateToChange = {};
     stateToChange[evt.target.id] = evt.target.value;
+
     this.setState(stateToChange)
     console.log(stateToChange);
   };
 
+
+
   saveNewLocation = evt => {
     evt.preventDefault();
+    
 
     const location = {
       address: this.state.address,
-      monday: this.state.monday,
-      tuesday: this.state.tuesday,
-      wednesday: this.state.wednesday,
-      thursday: this.state.thursday,
-      friday: this.state.friday,
-      saturday: this.state.saturday,
-      sunday: this.state.sunday
+      checkboxes: this.state.checkboxes
     };
 
     this.addLocation(location).then(() => this.props.history.push('/'));
@@ -55,65 +105,50 @@ export default class EventForm extends Component {
   render() {
     return (
       <React.Fragment>
-        <form className="eventForm">
           <div className="form-group">
-            <label htmlFor="address">Event Name</label>
-            <input
-              type="text"
-              required
-              className="form-control"
-              onChange={this.handleFieldChange}
-              id="address"
-              placeholder="Address"
-            />
+                        <label htmlFor="address">Address</label>
+                        <input type="text"
+                        required
+                        className="form-control"
+                        onChange={this.handleFieldChange}
+                        id="address"
+                        placeholder="Address"
+                        />
+                    </div>
+
+
+        <div className="container">
+        <div className="row mt-5">
+          <div className="col-sm-12">
+            <form onSubmit={this.handleFormSubmit}>
+              {this.createCheckboxes()}
+
+              <div className="form-group mt-2">
+                <button
+                  type="button"
+                  className="btn btn-outline-primary mr-2"
+                  onClick={this.selectAll}
+                >
+                  Select All
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-primary mr-2"
+                  onClick={this.deselectAll}
+                >
+                  Deselect All
+                </button>
+                <button type="submit" className="btn btn-primary"
+                onClick={this.saveNewLocation}>
+                  Save
+                </button>
+              </div>
+            </form>
           </div>
-          <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Monday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Tuesday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Wednesday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Thursday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Friday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Saturday
-                        </Label>
-                      </FormGroup>
-                      <FormGroup check>
-                        <Label check>
-                          <Input type="checkbox" />{' '}
-                          Sunday
-                        </Label>
-                      </FormGroup>
-          <button className="button" onClick={this.saveNewLocation}>
-            Submit
-          </button>
-        </form>
+        </div>
+      </div>
       </React.Fragment>
     );
   }
 }
+
