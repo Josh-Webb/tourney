b988872576fc06ba58d07b05ba14729ecbb5a0b8
Josh Webb <joshwebb42@gmail.com>
2019-06-29 02:38:29 -0500

    can build the array of addresses but can't grab the name.  Considering using singular array of objects instead of multiple arrays.

--- tourney/src/modules/Generator.js
+++ tourney/src/modules/Generator.js
@@ -5,6 +5,7 @@ import './LibraryManager';
 import './UserManager';
 import './DistanceMatrixManager';
 import './ParseGameId'
+import UserManager from "./UserManager"
 import ConnectionManager from "./ConnectionManager";
 import parseGameId from "./ParseGameId";
 import LibraryCard from '../components/library/libraryCard'
@@ -29,7 +30,105 @@ const generator = (gameId) => {
                      console.log("nope")
                 }
             }
+            // let mondayCounter = 0;
+            // let tuesdayCounter = 0;
+            // let wednesdayCounter = 0;
+            // let thursdayCounter = 0;
+            // let fridayCounter = 0;
+            // let saturdayCounter = 0;
+            // let sundayCounter = 0;
+            let mondayAddressesArray = [];
+            let tuesdayAddressesArray = [];
+            let wednesdayAddressesArray = [];
+            let thursdayAddressesArray = [];
+            let fridayAddressesArray = [];
+            let saturdayAddressesArray = [];
+            let sundayAddressesArray = [];
+            playerIdArray.forEach(function(element) {
+                console.log(element)
+                UserManager.getId(element)
+                .then(grabedUser => {
+                    console.log(grabedUser.checkboxes.monday)
+                    if (grabedUser.checkboxes.monday === true) {
+                        // mondayCounter += 1;
+                        mondayAddressesArray.push(grabedUser.address);
+                        // console.log("monday counter plus one.  Now equals >", mondayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.tuesday)
+                    if (grabedUser.checkboxes.tuesday === true) {
+                        // tuesdayCounter += 1;
+                        tuesdayAddressesArray.push(grabedUser.address)
+                        // console.log("tuesday counter plus one.  Now equals >", tuesdayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.wednesday)
+                    if (grabedUser.checkboxes.wednesday === true) {
+                        // wednesdayCounter += 1;
+                        wednesdayAddressesArray.push(grabedUser.address)
+                        // console.log("wednesday counter plus one.  Now equals >", wednesdayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.thursday)
+                    if (grabedUser.checkboxes.thursday === true) {
+                        // thursdayCounter += 1;
+                        thursdayAddressesArray.push(grabedUser.address)
+                        // console.log("thursday counter plus one.  Now equals >", thursdayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.friday)
+                    if (grabedUser.checkboxes.friday === true) {
+                        // fridayCounter += 1;
+                        fridayAddressesArray.push(grabedUser.address)
+                        // console.log("friday counter plus one.  Now equals >", fridayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.saturday)
+                    if (grabedUser.checkboxes.saturday === true) {
+                        // saturdayCounter += 1;
+                        saturdayAddressesArray.push(grabedUser.address)
+                        // console.log("saturday counter plus one.  Now equals >", saturdayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    console.log(grabedUser.checkboxes.sunday)
+                    if (grabedUser.checkboxes.sunday === true) {
+                        // sundayCounter += 1;
+                        sundayAddressesArray.push(grabedUser.address)
+                        // console.log("sunday counter plus one.  Now equals >", sundayCounter)
+                    } else {
+                        console.log("false")
+                    };
+                    // console.log(mondayCounter, tuesdayCounter, wednesdayCounter, thursdayCounter, fridayCounter, saturdayCounter, sundayCounter);
+                    // let dayCounter = [mondayCounter, tuesdayCounter, wednesdayCounter, thursdayCounter, fridayCounter, saturdayCounter, sundayCounter]
+                    // console.log(dayCounter, "daycounter array")
+                    console.log(mondayAddressesArray, "addresses array")
+                    console.log(tuesdayAddressesArray, "tuesday addresses")
+                    console.log(wednesdayAddressesArray, "wednesday addresses")
+                    console.log(thursdayAddressesArray, "thursday addresses")
+                    console.log(fridayAddressesArray, "friday addresses array")
+                    console.log(saturdayAddressesArray, "saturday addresses array")
+                    console.log(sundayAddressesArray, "sunday addresses array")
+                    // let allCountersArray = [mondayCounter, tuesdayCounter, wednesdayCounter, thursdayCounter, fridayCounter, saturdayCounter, sundayCounter]
+                    let allAddressesArray = [mondayAddressesArray, tuesdayAddressesArray, wednesdayAddressesArray, thursdayAddressesArray, fridayAddressesArray, saturdayAddressesArray, sundayAddressesArray]
+                    // let sortedCounterArray = (allCountersArray.sort(function(a, b){return b.length - a.length}))
+                    // console.log("All Counters Array", allCountersArray)\
+                    let orderedAddressesArray = ((allAddressesArray.sort(function(a,b){return b.length - a.length})))
+                    let winnerAddressesArray = orderedAddressesArray[0];
+                    let winnerName = orderedAddressesArray[0].keys();
+                    console.log("All Addresses Array", allAddressesArray)
+                    console.log((allAddressesArray.sort(function(a, b){return b.length - a.length})), "ordered with length")
+                    console.log("WINNER ADDRESSES", winnerAddressesArray)
+                    console.log(winnerName)
+            });
         })
+}
+        )
 }
 
 export default generator
